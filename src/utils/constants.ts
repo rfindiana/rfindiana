a22cd99396d4ae7d781e99e0cae49a4f8c8c70f1
Dale Brubaker <brubaker.dale@gmail.com>
2025-04-15 15:30:20 -0400

    Refactor some constants into constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const SITE = {
+  name: "Religious Freedom Indiana",
+  title: {
+    default: "Religious Freedom Indiana",
+    template: "%s | Religious Freedom Indiana",
+  },
+  description: "Hoosiers working for religious freedom.",
+} as const;
+
+export const SOCIAL = {
+  twitter: {
+    creator: "@surjithctly",
+    site: "@web3templates",
+    cardType: "summary_large_image",
+  },
+} as const;
+
+// Common text used across pages
+export const COMMON_TEXT = {
+  amazonNote:
+    "Note: Most videos listed here are available on Amazon Prime for a small fee or included with a subscription. Some may also be available for free elsewhere online.",
+} as const;
