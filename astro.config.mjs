1b842c0c2343bdabeab10b977b52bc1045ad7b8b
Dale Brubaker <brubaker.dale@gmail.com>
2025-04-15 15:30:20 -0400

    more refactoring of hard-coded variables into env.example

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: process.env.SITE_URL || "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
