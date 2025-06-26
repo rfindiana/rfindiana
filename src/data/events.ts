70f57f73db8fdcc7b34f50bbffd7984cc6e67333
Dale Brubaker <brubaker.dale@gmail.com>
2025-06-26 18:45:57 -0400

    change brainstorming meeting july 8 to july 7

--- src/data/events.ts
+++ src/data/events.ts
@@ -45,9 +45,9 @@ export const upcomingEvents: Event[] = [
   },
   {
     title: "Brainstorming session",
-    date: "2025-07-08",
+    date: "2025-07-07",
     time: "7:00 PM ET",
-    description: "As a new state-wide organization, we are still forming action plans to confront Christian nationalism. Join us on July 8th as we discuss next steps to achieve each of our goals:\n\n-   Educate the public\n-   Counter harmful disinformation\n-   Defend individuals and communities\n-   Foster dialogue across beliefs\n-   Inspire action and advocacy\n\nBring your ideas and show up on Zoom!",
+    description: "As a new state-wide organization, we are still forming action plans to confront Christian nationalism. Join us on July 7th as we discuss next steps to achieve each of our goals:\n\n-   Educate the public\n-   Counter harmful disinformation\n-   Defend individuals and communities\n-   Foster dialogue across beliefs\n-   Inspire action and advocacy\n\nBring your ideas and show up on Zoom!",
     location: "Zoom Meeting",
     registrationLink: "https://us06web.zoom.us/meeting/register/_b8izs5vSEyNRZENwmBeoQ"
   },
